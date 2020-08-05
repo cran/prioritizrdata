c87ecb4e54a2881562de0c135ec02839573b2370
Richard Schuster <richard.schuster@glel.carleton.ca>
2020-08-05 08:50:02 +0000

    version 0.2.4

--- R/tas_data.R
+++ R/tas_data.R
@@ -1,24 +1,28 @@
-#' Tasmanian conservation planning data
+#' Tasmania data
 #'
-#' This data set was obtained from the
-#' \href{http://marxan.net/courses}{"Introduction to \emph{Marxan}" course}
+#' This dataset was obtained from the
+#' ["Introduction to *Marxan*" course](https://marxansolutions.org/)
 #' and was originally part of a larger spatial prioritization performed under
 #' contract to Australia's Department of Environment and Water Resources
-#' (Klein \emph{et al.} 2007).
+#' (Klein *et al.* 2007). For a worked example with this
+#' dataset, refer to the [Tasmania vignette in the \pkg{prioritizr} package](https://CRAN.R-project.org/package=prioritizr/vignettes/tasmania.html).
 #'
-#' @details The data set contains the following items:
+#' @details The dataset contains the following items:
 #'
 #' \describe{
 #'
-#'   \item{\code{tas_pu}}{Planning unit data. The attribute table has
-#'     three columns containing unique identifiers ("id"), unimproved land
-#'     values ("cost"), and their existing level of protection ("status").
-#'     Units with 50 \% or more of their area contained in protected areas
-#'     are associated with a status of 2, otherwise they are associated with a
-#'     value of 0.}
+#'   \item{`tas_pu`}{Planning unit data. The attribute table has
+#'     three columns containing unique identifiers (`"id"`),
+#'     unimproved land values (`"cost"`), and their existing level of
+#'     protection (`"status"`). Units with 50% or more of their area
+#'     contained in protected areas are associated with a status of 2,
+#'     otherwise they are associated with a value of 0.
+#'     It also contains columns (`"locked_in"` and `"locked_out"`)
+#'     with `logical` values (i.e. `TRUE` or `FALSE` values)
+#'     for locking in and locking out planning units.}
 #'
-#'   \item{\code{tas_features}}{The distribution of 62 vegetation
-#'     classes in Tasmania, Australia. Each layer in the  stack
+#'   \item{`tas_features`}{The distribution of 62 vegetation
+#'     classes in Tasmania, Australia. Each layer in the stack
 #'     represents a different vegetation class. For a given layer,
 #'     cells indicate the presence (value of 1) or absence (value of 0)
 #'     of the vegetation class in an area.}
@@ -33,22 +37,26 @@
 #' @usage data(tas_pu)
 #'
 #' @format \describe{
-#'   \item{tas_features}{\code{\link[raster]{RasterStack-class}} object}
-#'   \item{tas_pu}{\code{\link[sp]{SpatialPolygonsDataFrame}} object.}
+#'   \item{tas_features}{[`RasterStack-class`] object}
+#'   \item{tas_pu}{[sp::SpatialPolygonsDataFrame()] object.}
 #' }
 #'
 #' @keywords datasets
 #'
 #' @references
-#' Klein C, Carwardine J, Wilson K, Watts M, and Possingham H (2007) \emph{Spatial
+#' Klein C, Carwardine J, Wilson K, Watts M, and Possingham H (2007) *Spatial
 #' Prioritization Approaches for the Conservation of Biodiversity in
 #' Australia: Considering Conservation Costs, Ecological & Evolutionary
-#' Processes, and Large-Intact Areas.} Report to the Department of
+#' Processes, and Large-Intact Areas.* Report to the Department of
 #' Environment; Water Resources.
 #'
 #' @examples
 #' # load data
 #' data(tas_pu, tas_features)
+#'
+#' # preview data
+#' print(tas_pu)
+#' print(tas_features)
 #'
 #' # plot data
 #' plot(tas_pu)
@@ -56,9 +64,3 @@
 #'
 #' @name tas_data
 NULL
-
-#' @rdname tas_data
-"tas_pu"
-
-#' @rdname tas_data
-"tas_features"
